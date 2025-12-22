04d6be60b24a04c361fb9973c7854a4a8252ded9
speedrower <fbak2592@gmail.com>
2025-12-22 16:53:53 +0100

    Translate month days program to English

--- daysInMonth.cpp
+++ daysInMonth.cpp
@@ -1,37 +1,37 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int month;
-    cout << "Podaj numer miesiaca (1-12): ";
+    cout << "Enter the month number (1-12): ";
     cin >> month;
 
     switch (month)
     {
     case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-        cout << "Ten miesiac ma 31 dni." << endl;
+        cout << "This month has 31 days." << endl;
         break;
 
     case 4: case 6: case 9: case 11:
-        cout << "Ten miesiac ma 30 dni." << endl;
+        cout << "This month has 30 days." << endl;
         break;
 
     case 2:
     {
         int year;
-        cout << "Podaj rok: ";
+        cout << "Enter the year: ";
         cin >> year;
 
         if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-            cout << "Ten miesiac ma 29 dni." << endl;
+            cout << "This month has 29 days." << endl;
         else
-            cout << "Ten miesiac ma 28 dni." << endl;
+            cout << "This month has 28 days." << endl;
         break;
     }
 
     default:
-        cout << "Nieprawidlowy numer miesiaca." << endl;
+        cout << "Invalid month number." << endl;
     }
 
     return 0;
